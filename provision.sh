fa5e957314af8c565b300c9866b61b527d27f4d7
Jesse Overright <jesseoverright@gmail.com>
2016-02-17 19:59:29 -0600

    Adds mongodb to vagrant box

--- provision.sh
+++ provision.sh
@@ -6,6 +6,14 @@
 sudo yum install -y epel-release
 sudo yum install -y nodejs npm
 
+# install mongodb
+# data - /var/lib/mongo
+# log - /var/log/mongodb/mongod.log
+sudo ln -s /vagrant/mongodb-org-3.0.repo /etc/yum.repos.d/
+sudo yum install -y mongodb-org
+sudo service mongod start
+sudo chkconfig mongod on
+
 # open up port 8080 to outside viewers
 sudo iptables -I INPUT -p tcp -m tcp --dport 8080 -j ACCEPT
 sudo service iptables save
